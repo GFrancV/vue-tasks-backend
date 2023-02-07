cd86ed5b6fccdf68050395a8937bd7e5e0a8866c
GFrancV <gfrancv@hotmail.com>
2023-02-07 14:37:23 +0000

    Init the api with users and tasks table.

---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model {
+	use HasFactory;
+
+	protected $fillable = [
+		'id',
+		'title',
+		'description',
+		'user_id',
+	];
+
+	/**
+	 * Get the user that owns the Task
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function user() {
+		return $this->belongsTo(User::class, 'id');
+	}
+}
