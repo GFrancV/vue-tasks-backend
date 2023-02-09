17b2e967c158fe25bfe73f3f88e9d183e0ddd61f
GFrancV <gfrancv@hotmail.com>
2023-02-09 17:53:12 +0000

    feat(auth): Now the tasks routes are only available to auth users.

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-	return response()->json('welcome', 200);
-});
-
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout']);
